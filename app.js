b1c1ee78c9152fcb73034cb5ece9d3c263fa95c8
George Uweh <59946617+GAMHnile@users.noreply.github.com>
2020-07-26 18:14:31 +0100

    added eslint rules, created bookRouter, added books.ejs

--- app.js
+++ app.js
@@ -1,20 +1,43 @@
 const express = require('express');
-const debug = require('debug');
+const debug = require('debug')('app');
 const chalk = require('chalk');
 const path = require('path');
 const morgan = require('morgan');
 
-
 const app = express();
+const port = process.env.PORT || 3000;
+
+const bookRouter = require('./src/routes/bookRoute');
+
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, 'public')));
+// app.use(
+//   '/css',
+//   express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')),
+// );
+app.use(
+  '/js',
+  express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')),
+);
+app.use(
+  '/js',
+  express.static(path.join(__dirname, 'node_modules/jquery/dist')),
+);
 
+app.use('/books', bookRouter);
 
-app.get('/', (req, res)=>{
-    res.sendFile(path.join(__dirname, '/views', 'index.html'));
-})
+app.get('/', (req, res) => {
+  res.render('index', {
+    nav: [
+      { title: 'Books', link: '/books' },
+      { title: 'Authors', link: '/authors' }
+    ],
+    title: 'Library'
+  });
+});
 
-app.listen(3000,()=>{
-    debug(`App running on port ${chalk.green(3000)}`);
-})
-
+app.listen(port, () => {
+  debug(`App running on port ${chalk.green(port)}`);
+});
